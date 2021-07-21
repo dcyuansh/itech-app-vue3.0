b0d4d07da5aca27325376fd8a8b79e444d5c00d8
De Chun Yuan <yuandechunsh@163.com>
2021-07-21 12:50:02 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,17 @@ console.log(path.resolve(__dirname, "./src"));
 
 module.exports = {
   alias: {
-    // 键必须以斜线开始和结束
-    "/@/": path.resolve(__dirname, "./src"),
+    "@": path.resolve(__dirname, "./src"),
   },
 
   //在生产中服务时的基本公共路径。 @default '/'
   base: "./",
   // 与“根”相关的目录，构建输出将放在其中。如果目录存在，它将在构建之前被删除。 @default 'dist'
   outDir: "dist",
+  // 输出的静态资源的文件夹名称
+  assetsDir: 'assets',
   //端口号
-  port: 8080,
+  port: 3000,
   // 是否自动在浏览器打开
   open: false,
   // 是否开启 https
@@ -25,19 +26,19 @@ module.exports = {
     include: ["element-plus/lib/locale/lang/zh-cn"],
   },
 
-  server: {
-    proxy: {
-      "/api": {
-        target: 'http://106.14.202.126:8443',//代理地址，这里设置的地址会代替axios中设置的baseURL
-        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
-        pathRewrite: {
-          '^/api': '/api'
-          //pathRewrite: {'^/api': '/'} 重写之后url为 http://localhost:8443/xxxx
-          //pathRewrite: {'^/api': '/api'} 重写之后url为 http://localhost:8443/api/xxxx
-        }
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: 'http://106.14.202.126:8443',//代理地址，这里设置的地址会代替axios中设置的baseURL
+  //       changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+  //       pathRewrite: {
+  //         '^/api': '/api'
+  //         //pathRewrite: {'^/api': '/'} 重写之后url为 http://localhost:8443/xxxx
+  //         //pathRewrite: {'^/api': '/api'} 重写之后url为 http://localhost:8443/api/xxxx
+  //       }
+  //     },
+  //   },
+  // },
 
 
 };
